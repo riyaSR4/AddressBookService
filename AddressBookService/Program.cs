02e202931a37c2ca7a80fa2fe0550c520db20b40
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-08-31 12:55:10 +0530

    [Riya] Add : Address Book

---
+++ AddressBookService/Program.cs
@@ -0,0 +1,43 @@
+﻿using AddressBookService;
+using System;
+using System.Net;
+
+namespace AddressBookService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AddressBook contact = new AddressBook()
+            //{
+            //    FirstName = "Riya S",
+            //    LastName = "Reji",
+            //    Address = "Mylapra",
+            //    City = "Pathanamthitta",
+            //    State = "Kerala",
+            //    Zip = 123456,
+            //    PhoneNumber = "1234567890",
+            //    Email = "riya@gmail.com"
+            //};
+            Operations operations = new Operations();
+            //operations.AddContact(contact);
+            //AddressBook contact1 = new AddressBook()
+            //{
+            //    FirstName = "Riya",
+            //    LastName = "Reji",
+            //    Address = "Mylapra",
+            //    City = "Pathanamthitta",
+            //    State = "Kerala",
+            //    Zip = 123456,
+            //    PhoneNumber = "1234567890",
+            //    Email = "riya@gmail.com"
+            //};
+            //operations.UpdateContact(contact1);
+            //operations.DeleteContact("riya");
+            //operations.City("Pathanamthitta");
+            //operations.State("Kerala");
+            //operations.SizeByCity();
+            operations.SizeByState();
+        }
+    }
+}
